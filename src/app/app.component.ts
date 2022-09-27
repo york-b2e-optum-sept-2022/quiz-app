c4ae7daac9c23f8a55cbba0593358ee8ad19d134
adam-street <adam.street.dev@gmail.com>
2022-09-27 15:18:17 -0500

    added unsub from dataService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {DataService, STATE} from "./data.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'quiz-app';
   state: STATE;
 
+  sub: Subscription;
+
   constructor(private dataService: DataService) {
     this.state = this.dataService.state;
-    this.dataService.$state.subscribe(
+    this.sub = this.dataService.$state.subscribe(
       (newState) => this.state = newState
     )
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
